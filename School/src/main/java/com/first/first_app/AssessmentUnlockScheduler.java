52926f7195b7cf593727f883d2d101490ce3bbae
Mariamatef123 <154092598+Mariamatef123@users.noreply.github.com>
2026-06-21 04:32:54 +0300

    formating

--- School/src/main/java/com/first/first_app/AssessmentUnlockScheduler.java
+++ School/src/main/java/com/first/first_app/AssessmentUnlockScheduler.java
@@ -7,6 +7,7 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.first.first_app.Enum.StudentStatus;
 import com.first.first_app.Enum.Term;
 import com.first.first_app.Model.Student;
 import com.first.first_app.Repo.StudentRepo;
@@ -18,7 +19,7 @@ public class AssessmentUnlockScheduler {
 
     private final StudentRepo studentRepo;
 
-    @Scheduled(cron = "0 * * * * *")
+    @Scheduled(cron = "* * * * * *")
     @Transactional
     public void unlockAssessments() {
         LocalDate today = LocalDate.now();
@@ -43,7 +44,7 @@ public class AssessmentUnlockScheduler {
             
             if (shouldUnlock) {
                 student.setUnlockedAssessmentCount(nextAssessmentToUnlock);
-                
+                student.setStatus(StudentStatus.PENDING);
                
                 if (nextAssessmentToUnlock >= 6) {
                     student.setAssessmentsLocked(false);
@@ -59,8 +60,8 @@ public class AssessmentUnlockScheduler {
     private boolean shouldUnlockAssessment(Term term, int assessmentNumber, int year, int month, int day) {
         if (term == Term.TERM_1) {
             switch (assessmentNumber) {
-                case 1: return (year == 2026 && month == 6 && day >= 17);
-                case 2: return (year == 2026 && month == 10 && day >= 20);
+                case 1: return (year == 2026 && month == 9 && day >= 20);
+                case 2: return (year == 2026 && month == 10 && day >=20);
                 case 3: return (year == 2026 && month == 11 && day >= 20);
                 case 4: return (year == 2026 && month == 12 && day >= 20);
                 case 5: return (year == 2027 && month == 1 && day >= 10);
@@ -72,7 +73,7 @@ public class AssessmentUnlockScheduler {
                 case 1: return (year == 2027 && month == 2 && day >= 25);
                 case 2: return (year == 2027 && month == 3 && day >= 25);
                 case 3: return (year == 2027 && month == 4 && day >= 25);
-                case 4: return (year == 2027 && month == 5 && day >= 20);
+                case 4: return (year == 2026 && month == 6 && day >= 21);
                 case 5: return (year == 2027 && month == 5 && day >= 30);
                 case 6: return (year == 2027 && month == 6 && day >= 10);
                 default: return false;
